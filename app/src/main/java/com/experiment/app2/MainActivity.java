a26ffcf796a301fe6bfdb2aba5d5f9dc64f61703
Nitish D <nitishdevadiga@hotmail.com>
2020-08-18 21:20:58 +0530

    Style changes, styling features added.

--- app/src/main/java/com/experiment/app2/MainActivity.java
+++ app/src/main/java/com/experiment/app2/MainActivity.java
@@ -1,10 +1,16 @@
 package com.experiment.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -15,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private MaterialToolbar toolbar;
     private TextView input;
     private TextView output;
-    private ConstraintLayout parentLayout;
+    private LinearLayout parentLayout;
+    private CheckBox boldCheck, italicCheck, bigCheck;
+    private RadioGroup backgroundColorRadio;
+    private float textSizeBig, textSizeNormal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +39,76 @@ public class MainActivity extends AppCompatActivity {
         parentLayout = findViewById(R.id.parentLayout);
         input = findViewById(R.id.input);
         output = findViewById(R.id.output);
+        boldCheck = findViewById(R.id.boldCheck);
+        italicCheck = findViewById(R.id.italicCheck);
+        bigCheck = findViewById(R.id.bigCheck);
+        backgroundColorRadio = findViewById(R.id.radioGroup);
+
+        textSizeBig = getResources().getDimension(R.dimen.bigOutputSize) / getResources().getDisplayMetrics().density;
+        textSizeNormal = getResources().getDimension(R.dimen.normalOutputSize) / getResources().getDisplayMetrics().density;
+
+        boldCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    output.setTypeface(null, Typeface.BOLD);
+                    if(italicCheck.isChecked())
+                        output.setTypeface(null, Typeface.BOLD_ITALIC);
+                } else {
+                    output.setTypeface(null, Typeface.NORMAL);
+                    if(italicCheck.isChecked())
+                        output.setTypeface(null, Typeface.ITALIC);
+                }
+            }
+        });
+
+        italicCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    output.setTypeface(null, Typeface.ITALIC);
+                    if(boldCheck.isChecked())
+                        output.setTypeface(null, Typeface.BOLD_ITALIC);
+                } else {
+                    output.setTypeface(null, Typeface.NORMAL);
+                    if(boldCheck.isChecked())
+                        output.setTypeface(null, Typeface.BOLD);
+                }
+            }
+        });
+
+        bigCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    output.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeBig);
+                } else {
+                    output.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeNormal);
+                }
+            }
+        });
+
+        backgroundColorRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()){
+                    case R.id.purpleSelect:
+                        output.setBackgroundColor(Color.parseColor("#9C27B0"));
+                        break;
+                    case R.id.redSelect:
+                        output.setBackgroundColor(Color.parseColor("#9B0B0B"));
+                        break;
+                    case R.id.blueSelect:
+                        output.setBackgroundColor(Color.parseColor("#03A9F4"));
+                        break;
+                }
+            }
+        });
     }
 
     public void fact(View v){
         long n = (long) getInput();
-        String result = "";
+        String result;
         if (n > 25)
             result = "Factorial too big to display!";
         else if (n < 0)
@@ -52,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void evenOdd(View v){
         int n = (int) getInput();
-        String result = "";
+        String result;
         if (n%2 == 0) {
             result =  n + " is even";
         } else {
@@ -63,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkPrime(View v){
         int n = (int) getInput();
-        String result = "";
+        String result;
         boolean isFact = true;
         if (n<2)
             isFact = false;
@@ -82,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkArmstrong(View v){
         long n = (long) getInput();
-        String result = "";
+        String result;
         long temp = n;
         long sum = 0;
         int digits = getDigits(n);
